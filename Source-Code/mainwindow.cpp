a9b3e5f9bca545dd8b0299e2293c1c6710df3cb8
Youssef <therealyoussef@aucegypt.edu>
2016-11-21 03:46:17 +0200

    Fixed include

--- Source-Code/mainwindow.cpp
+++ Source-Code/mainwindow.cpp
@@ -1,7 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <qfiledialog.h>
-#include <circleandrectangledetector.h>
+#include "circleandrectangledetector.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
